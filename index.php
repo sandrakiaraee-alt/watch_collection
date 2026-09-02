ae6bd9733bc3f93b9c7ddf878e5b7f044db8d1c0
Onyeka Chisom <splashanthony5@gmail.com>
2026-09-02 14:34:56 +0100

    I worked on the landing page: I added a images

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+use Bramus\Router\Router;
+require __DIR__ . '/vendor/autoload.php';
---
+++ ui/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>watch collection</title>
+</head>
+
+<body>
+    <section class="hero">
+
+        <div class="hero-text">
+            <p>NEW PRODUCT</p>
+
+            <h1>
+                WATCH<br>
+                COLLECTION
+            </h1>
+
+            <p>
+                Discover premium watches designed
+                for style and precision.
+            </p>
+
+            <a href="http://google.com"><button class="product-btn">SEE PRODUCT</button></a>
+        </div>
+
+        <div class="hero-image">
+            <img src="./Images/watch.png" alt="Premium Watch">
+        </div>
+
+    </section>
+    <section>
+
+        <div class="first-img">
+            <img src="./Images/pngwing.com (8).png" alt="hamilton">
+            <img src="./Images/pngwing.com (8).png" alt="watch">
+            <img src="./Images/pngwing.com (8).png" alt="senken">
+            <img src="./Images/pngwing.com (8).png" alt="omega">
+            <img src="./Images/pngwing.com (8).png" alt="cartier">
+            <img src="./Images/pngwing.com (8).png" alt="U-boat">
+            <img src="./Images/pngwing.com (8).png" alt="G-shock">
+            <img src="./images/pngwing.com (8).png" alt="apple-watch">
+        </div>
+
+        <div class="second-img">
+            <img src="./Images/pngwing.com (8).png" alt="hamilton">
+            <img src="./Images/pngwing.com (8).png" alt="watch">
+            <img src="./Images/pngwing.com (8).png" alt="senken">
+            <img src="./Images/pngwing.com (8).png" alt="omega">
+            <img src="./Images/pngwing.com (8).png" alt="cartier">
+            <img src="./Images/pngwing.com (8).png" alt="U-boat">
+            <img src="./Images/pngwing.com (8).png" alt="G-shock">
+            <img src="./images/pngwing.com (8).png" alt="apple-watch">
+
+        </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
